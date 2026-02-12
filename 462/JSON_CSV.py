ab536c2b934545bba250f9453e5e347087f7ed28
SaimSiddique1 <saimsiddique728@gmail.com>
2026-02-12 12:08:29 -0500

    Successfully transformed JSON to CSV

--- 462/JSON_CSV.py
+++ 462/JSON_CSV.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 
 # Load the JSON file (use the exact filename you provided)
-with open("whitetail_deer_dataset (1) (1).json", "r") as f:
+with open("whitetail_deer_dataset.json", "r") as f:
     data = json.load(f)
 
 # Convert JSON data into a DataFrame
